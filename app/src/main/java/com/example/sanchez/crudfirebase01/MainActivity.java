9f9c167f8c887c261e69f64e53ab261b98b94eae
Alberto017 <xavihdz09@gmail.com>
2020-03-28 15:43:52 -0700

    feat: Create graphic interface

--- app/src/main/java/com/example/sanchez/crudfirebase01/MainActivity.java
+++ app/src/main/java/com/example/sanchez/crudfirebase01/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.sanchez.crudfirebase01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.icon_add: {
+                Toast.makeText(getApplicationContext(),"Item add",Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case  R.id.icon_save: {
+                Toast.makeText(getApplicationContext(),"Item save",Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.icon_delete: {
+                Toast.makeText(getApplicationContext(),"Item delete",Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }//switch
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/sanchez/crudfirebase01/model/Person.java
@@ -0,0 +1,51 @@
+package com.example.sanchez.crudfirebase01.model;
+
+public class Person {
+
+
+    public String id;
+    public String name;
+    public String lastName;
+    public String email;
+
+    public Person() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
